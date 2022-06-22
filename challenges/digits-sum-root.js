d6642b600476618e893dfa0c3517498b7087a0d9
THOMAS HARDING <hardingt604@gmail.com>
2022-06-22 13:18:29 -0600

    add digits-sum-root

---
+++ challenges/digits-sum-root.js
@@ -0,0 +1,14 @@
+function rootDigit(n) {
+    console.log
+    if (n < 10) {
+        return n;
+    }
+    return rootDigit((n % 10) + rootDigit(Math.floor(n / 10)));
+  }
+  
+console.log(rootDigit(123));
+// 6
+console.log(rootDigit(4322));
+//  2
+console.log(rootDigit(999888777));
+//   // 9
