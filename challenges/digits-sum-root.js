823bf7991a66af2a28a9c5fc9fbb39225f1370b6
THOMAS HARDING <hardingt604@gmail.com>
2022-06-22 13:48:56 -0600

    add alternate solution to digits-sum-root using provided hints in instruction file

--- challenges/digits-sum-root.js
+++ challenges/digits-sum-root.js
@@ -2,12 +2,16 @@ function rootDigit(n) {
     if (n < 10) {
         return n;
     }
-    return rootDigit((n % 10) + rootDigit(Math.floor(n / 10)));
+    const digits = n.toString().split('');
+    const numbers = digits.map(d => +d);
+    const sum = numbers.reduce((a, b) => a + b);
+    return (rootDigit(sum));
+    // return rootDigit((n % 10) + rootDigit(Math.floor(n / 10)));
   }
   
 console.log(rootDigit(123));
 // 6
 console.log(rootDigit(4322));
-//  2
+// 2
 console.log(rootDigit(999888777));
-//   // 9
+//  9
